ef05dd34fe4822f9f1245a11033a756854e6f7f0
Nick Kazakov <nick.kazakov@gmail.com>
2016-08-11 11:48:36 +0300

    Direction at

--- src/com/company/Direction.java
+++ src/com/company/Direction.java
@@ -2,11 +2,22 @@ package com.company;
 
 public class Direction {
 
-    public int degrees;
+    public static final double DEFAULT_THRESHOLD = 0.02;
 
-    public Direction(int degrees) {
-        this.degrees = degrees;
+    public double angle;
+
+    public Direction(double angle) {
+        this.angle = angle;
     }
 
     public Direction() {}
+
+    public boolean at(Point from, Point to) {
+        return at(from, to, DEFAULT_THRESHOLD);
+    }
+
+    public boolean at(Point from, Point to, double threshold) {
+        double realAngle = from.angle(to);
+        return Math.abs(angle - realAngle) < threshold; // TODO case when over Pi
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,25 +2,50 @@ package com.company;
 
 public class Main {
 
+    Point p1 = new Point(10, 20);
+    Point p2 = new Point(15, 2);
+    Point p1x = new Point(10, 30);
+    Point p1y = new Point(30, 20);
+
     public static void main(String[] args) {
-        test1();
+        new Main();
     }
 
-    public static void test1() {
-        Point p1 = new Point(10, 20);
-        Point p2 = new Point(15, 2);
-        Point p1x = new Point(10, 30);
-        Point p1y = new Point(30, 20);
+    public Main() {
+        log("\n");
+        testDistance();
+        testAngle();
+        testDirection();
+        log("");
+    }
+
+    public void testDistance() {
         log("distance p1 p2: " + p1.distance(p2));
+        log("distance p1 p1x: " + p1.distance(p1x));
+        log("distance p1 p1y: " + p1.distance(p1y));
+        log("");
+    }
+
+    public void testAngle() {
         log("angle p1 p2: " + p1.angle(p2));
         log("angle p2 p1: " + p2.angle(p1));
         log("angle p1 p1x: " + p1.angle(p1x));
         log("angle p1x p1: " + p1x.angle(p1));
         log("angle p1 p1y: " + p1.angle(p1y));
         log("angle p1y p1: " + p1y.angle(p1));
+        log("");
     }
 
-    public static void log(String s) {
+    public void testDirection() {
+        log("direction 0.23: " + new Direction(0.23).at(p1, p2));
+        log("direction 0.252: " + new Direction(0.252).at(p1, p2));
+        log("direction 0.27: " + new Direction(0.27).at(p1, p2));
+        log("direction 0.29: " + new Direction(0.29).at(p1, p2));
+        log("direction 0.31: " + new Direction(0.31).at(p1, p2));
+        log("");
+    }
+
+    public void log(String s) {
         System.out.println(s);
     }
 }
