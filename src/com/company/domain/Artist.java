cfd371635b769ad061bfe0e9c7c09afa03ada517
Nick Kazakov <nick.kazakov@gmail.com>
2017-06-14 11:25:42 +0300

    functional examples

---
+++ src/com/company/domain/Artist.java
@@ -0,0 +1,27 @@
+package com.company.domain;
+
+public class Artist {
+
+    private String name;
+    private String from;
+
+    public Artist() {}
+
+    public Artist(String name, String from) {
+        this.name = name;
+        this.from = from;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public boolean isFrom(String from) {
+        //System.out.println(this.from + " vs " + from + ": " + this.from.equals(from));
+        return this.from.equals(from);
+    }
+}
